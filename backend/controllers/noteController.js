c09c8529410729fa53b5b642d4ec157618299fd3
Dimterion <dimterion59@gmail.com>
2023-02-15 17:05:50 +0100

    add get and create notes to backend

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -6,14 +6,46 @@ const Note = require("../models/noteModel");
 // @route   GET /api/notes
 // @access  Private
 const getNotes = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "getNotes" });
+  // Get user with the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const notes = await Note.find({ user: req.user.id });
+
+  res.status(200).json(notes);
 });
 
 // @desc    Create new note
 // @route   POST /api/notes
 // @access  Private
 const createNote = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "createNote" });
+  const { type, description } = req.body;
+
+  if (!type || !description) {
+    res.status(400);
+    throw new Error("Please add a type and description");
+  }
+
+  // Get user with the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const note = await Note.create({
+    type,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+
+  res.status(201).json(note);
 });
 
 module.exports = {
