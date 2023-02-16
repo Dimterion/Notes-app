41e6bc8a2be114f0132f0e81f1e4cc0f8abb2eb8
Dimterion <dimterion59@gmail.com>
2023-02-16 12:06:36 +0100

    add new note form

--- frontend/src/pages/NewNote.jsx
+++ frontend/src/pages/NewNote.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function NewNote() {
-  return <div>NewNote</div>;
+  const { user } = useSelector((state) => state.auth);
+  const [name] = useState(user.name);
+  const [type, setType] = useState("Task");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>Create New Note</h1>
+        <p>Fill out the form below</p>
+      </section>
+      <section className="form">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" value={name} disabled />
+        </div>
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="type">Type</label>
+            <select
+              name="type"
+              id="type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="Task">Task</option>
+              <option value="Thought">Thought</option>
+              <option value="Quote">Quote</option>
+              <option value="Event">Event</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description of the note</label>
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Create</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default NewNote;
