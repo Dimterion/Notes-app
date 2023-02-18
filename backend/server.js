36b550aa752bf0c5e75d5f17c4ea94a5d64e9b0c
Dimterion <dimterion59@gmail.com>
2023-02-18 10:24:26 +0100

    server.js fix for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const path = require("path");
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
+
+const express = require("express");
+const connectDB = require("./config/db");
+
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+require("colors");
+require("dotenv").config();
 
 // Database connection
 connectDB();
@@ -23,11 +26,11 @@ if ((process.env.NODE_ENV = "production")) {
   // Set builder folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  );
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 } else {
-  app.get("/", (req, res) => {
+  app.get("/", (_, res) => {
     res.status(200).json({ message: "Welcome to the Notes App API" });
   });
 }
